f79733340b1b18ae418ae48937f00b0f66211fc3
Robert Ratemo <robertratemo827@gmail.com>
2026-09-10 10:01:43 +0300

    readline callbacks

---
+++ checkresult.js
@@ -0,0 +1,19 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+const checkResult = (mark) => {
+    if (mark >= 50){
+        return "pass";
+    }
+     return "Fail";
+};
+rl.question('enter student name', (name) => {
+    rl.question('enter student mark', (mark) => {
+        const marks=Number(mark);
+        const result = checkResult(mark);
+        console.log (`${name}: ${result}`);
+        rl.close();
+    });
+});
